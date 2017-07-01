286c2f1edd581a990be583c421215c84aaa0edaf
Johan Kämpe <kampe.johan@gmail.com>
2017-07-01 23:31:59 +0200

    Init

---
+++ Deck.cs
@@ -0,0 +1,93 @@
+﻿
+using System.Collections.Generic;
+
+namespace BlackjackCS
+{
+    class Card
+    {
+        private int value;
+        private int suit;
+        private int cardId;
+
+        public Card(int value, int suit, int id)
+        {
+            this.value = value;
+            this.suit = suit;
+            this.cardId = id;
+        }
+
+        static public readonly int maxValue = 13;
+        static public readonly int maxSuit = 4;
+
+        public readonly string[] valueName = {
+            "Ace",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten",
+            "Jack",
+            "Queen",
+            "King"
+        };
+
+        public readonly string[] suitName = {
+            "Hearts",
+            "Diamonds",
+            "Clubs",
+            "Spades"
+        };
+
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}",
+                valueName[this.value],
+                suitName[this.suit]);
+        }
+
+        public int GetId()
+        {
+            return cardId;
+        }
+
+        public int GetValue(bool high = true)
+        {
+            int retval;
+
+            /* Ace */
+            if (this.value == 0)
+                retval = high ? 11 : 1;
+
+            else
+                retval = this.value > 9 ? 10 : this.value + 1;
+
+            return retval;
+        }
+    }
+
+    class Deck
+    {
+        public List<Card> card;
+
+        public Deck(int numberOfDecks = 1)
+        {
+            card = new List<Card>();
+            int cardId = 0;
+
+            for (int i = 0; i < numberOfDecks; i++)
+            {
+                for (int suit = 0; suit < Card.maxSuit; suit++)
+                {
+                    for (int value = 0; value < Card.maxValue; value++)
+                    {
+                        card.Add(new Card(value, suit, cardId++));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace BlackjackCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Create and print 4 decks */
+            Deck deck = new Deck(4);
+
+            foreach (Card card in deck.card)
+            {
+                Console.Write("{0:000} ", card.GetId());
+                Console.Write(card.ToString());
+                Console.WriteLine(" Value: {0}", card.GetValue());
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
