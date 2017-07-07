4d4b7c43833c35bce8a2ce22f87f4e276671e549
Johan Kämpe <kampe.johan@gmail.com>
2017-07-07 22:00:17 +0200

    Test natural blackjack scoring

--- Program.cs
+++ Program.cs
@@ -13,53 +13,23 @@ namespace BlackjackCS
             Deck deck = new Deck(4);
             deck.Shuffle();
 
-            Hand player1 = new Hand();
-            Hand player2 = new Hand();
-            Hand player3 = new Hand();
-            Hand player4 = new Hand();
-            Hand dealer = new Hand();
+            Hand hand = new Hand();
 
-            for (int i = 0; i < 2; i++)
+            /* Check natural blackjack */
+
+            int count = 0;
+
+            do
             {
-                player1.Add(deck.Deal());
-                player2.Add(deck.Deal());
-                player3.Add(deck.Deal());
-                player4.Add(deck.Deal());
-                dealer.Add(deck.Deal());
-            }
+                count++;
+                hand.Clear();
+                hand.Add(deck.Deal());
+                hand.Add(deck.Deal());
 
-            int high, low;
+            } while (!hand.NaturalBlackjack());
 
-            Console.WriteLine("\nPlayer 1:");
-            Console.WriteLine(player1.ShowCard(0).ToString());
-            Console.WriteLine(player1.ShowCard(1).ToString());
-            player1.Value(out high, out low);
-            Console.WriteLine("Score: " + low + " / " + high);
-
-            Console.WriteLine("\nPlayer 2:");
-            Console.WriteLine(player2.ShowCard(0).ToString());
-            Console.WriteLine(player2.ShowCard(1).ToString());
-            player2.Value(out high, out low);
-            Console.WriteLine("Score: " + low + " / " + high);
-
-            Console.WriteLine("\nPlayer 3:");
-            Console.WriteLine(player3.ShowCard(0).ToString());
-            Console.WriteLine(player3.ShowCard(1).ToString());
-            player3.Value(out high, out low);
-            Console.WriteLine("Score: " + low + " / " + high);
-
-            Console.WriteLine("\nPlayer 4:");
-            Console.WriteLine(player4.ShowCard(0).ToString());
-            Console.WriteLine(player4.ShowCard(1).ToString());
-            player4.Value(out high, out low);
-            Console.WriteLine("Score: " + low + " / " + high);
-
-            Console.WriteLine("\nDealer:");
-            Console.WriteLine(dealer.ShowCard(0).ToString());
-            Console.WriteLine(dealer.ShowCard(1).ToString());
-            dealer.Value(out high, out low);
-            Console.WriteLine("Score: " + low + " / " + high);
-
+            Console.WriteLine("Tries: " + count);
+            Console.WriteLine(hand.ToString());
             Console.ReadLine();
         }
     }
