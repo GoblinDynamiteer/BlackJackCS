0afb05e1b05ed84d772241dca45d81ebdcf02001
Johan Kämpe <kampe.johan@gmail.com>
2017-07-07 22:00:06 +0200

    Rewrote value calculation/return functions

--- Players.cs
+++ Players.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections.Generic;
 using PlayingCards;
-using System.Linq;
+using System;
 
 namespace BlackJackPlayers
 {
     class Hand
     {
         List<Card> cards;
+        List<Card> history;
         int valueHigh, valueLow;
+        bool naturalBlackjack;
 
         /* Constructor */
         public Hand()
         {
             cards = new List<Card>();
+            history = new List<Card>();
             valueLow = 0;
             valueHigh = 0;
+            naturalBlackjack = false;
         }
 
         /* Add card to player hand
@@ -35,31 +39,32 @@ namespace BlackJackPlayers
         private void SetValues()
         {
             valueHigh = valueLow = 0;
-
-            /* Probably unneeded to check for aces.... */
-            if (this.HasAces())
+            foreach (Card card in this.cards)
             {
-                foreach (Card card in this.cards)
-                {
-                    valueLow += card.GetValue(false);
-                    valueHigh += valueHigh + card.GetValue() < 21 ? 
-                        card.GetValue() : card.GetValue(false);
-                }
+                valueLow += card.GetValue(false);
+                valueHigh += valueHigh + card.GetValue() < 21 ? 
+                    card.GetValue() : card.GetValue(false);
             }
 
-            else
-            {
-                valueHigh = valueLow = this.cards.Sum(
-                    p => p.GetValue());
-            }
+            this.naturalBlackjack = 
+                (this.cards.Count == 2 && valueHigh == 21);
         }
 
-        public bool Value(out int high, out int low)
+        /* Returns low/high */
+        public int ValueHigh()
         {
-            high = valueHigh;
-            low = valueLow;
+            return valueHigh;
+        }
 
-            return (this.cards.Count == 2 && valueHigh == 21);
+        /* Returns low/high */
+        public int ValueLow()
+        {
+            return valueLow;
+        }
+
+        public bool NaturalBlackjack()
+        {
+            return naturalBlackjack;
         }
 
         /* Determine if the hand contains ace(s) */
@@ -70,12 +75,28 @@ namespace BlackJackPlayers
                     (int)Card.Value.AceHigh));
         }
 
+        /* Clear hand and copy cards to history */
+        public void Clear()
+        {
+            history.AddRange(cards);
+            cards.Clear();
+        }
+
+        public override string ToString()
+        {
+            string retval = "Hand:\n";
+            this.cards.ForEach(c => retval += c.ToString() + "\n");
+            return retval;
+        }
+
         /* Return card at hand index */
         public Card ShowCard(int index)
         {
             /* Check if correct... */
             if (index >= cards.Count)
             {
+                /* Fix */
+                Console.WriteLine("DECK EMPTY!");
                 return null;
             }
 
